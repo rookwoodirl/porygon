89c48154bb58ac1a78711ff1859098b889e563d4
rookwoodirl <c.carmellini@fetchrewards.com>
2023-12-02 17:32:31 -0500

    a bit of organization on the keys, but now a functioning chatbot with a prompt that is useful for talking with partneres. Falls asleep when not spoken to in a bit, wakes up when called by name (pory, or porygon).

--- porygon.py
+++ porygon.py
@@ -3,9 +3,12 @@ from discord.ext import commands
 from commands.chess_gif import pgn_to_gif
 from commands.factorio_blueprint import BlueprintImageConstructor
 import os
+from openai import OpenAI
+with open(os.path.join('api_keys', 'chatgpt.key'), 'r') as f:
+    chat_client = OpenAI(api_key=f.readline())
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
-with open('discord.key', 'r') as f:
+with open(os.path.join('api_keys', 'discord.key'), 'r') as f:
     BOT_TOKEN = f.readline().strip()
 
 def log(command, text):
@@ -51,9 +54,17 @@ async def fbp(ctx, *blueprint):
     for img in imgs:
         await ctx.send(file=discord.File(os.path.join('commands', img)))
 
+
+gpt_activated = False
+gpt_pass_counter = 0
+
 # Event handler for when a message is received
 @bot.event
 async def on_message(message):
+
+    global gpt_activated
+    global gpt_pass_counter
+
     # Ignore messages from the bot itself to prevent infinite loops
     if message.author == bot.user:
         return
@@ -64,6 +75,44 @@ async def on_message(message):
 
     # Process commands
     await bot.process_commands(message)
+
+    if 'pory' in message.content.lower() or '<@270372309273023273 >' in message.content:
+        gpt_activated = True
+
+    if gpt_pass_counter >= 5:
+        gpt_activated = False
+
+    if gpt_activated and len(message.content) < 1000:
+        # chatgpt
+        gpt_channels = ['日本語', 'italiano', 'deutsch', '한국어', 'español', 'bot-spam']
+        with open(os.path.join('assets', 'chatgpt', 'languages.prompt')) as f:
+            prompt = [
+                {
+                    "role": "system", 
+                    "content": '\n'.join(f.readlines())
+                }
+            ]
+        if message.channel.name in gpt_channels:
+            new_prompt = prompt.copy()
+            messages = [msg async for msg in message.channel.history(limit=10)]
+            for message in reversed(messages):
+                new_prompt.append({"role": "assistant" if message.author.display_name == "Porygon" else "user", "content": message.content[:2000]})
+
+            
+
+            response = chat_client.chat.completions.create(model="gpt-4",  messages=new_prompt)
+            reply = response.choices[0].message.content
+            if reply.lower()[:4] != "pass":
+                gpt_pass_counter = 0
+                await message.channel.send(reply)
+            else:
+                gpt_pass_counter += 1
+
+            if gpt_pass_counter == 5:
+                await message.channel.send("Zzzzz...... so sleepy....")
+
+
+
 
 
 # Start the bot
