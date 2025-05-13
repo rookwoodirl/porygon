b084d9e4d57c9d95fa21060037afe457a65f8c1e
rookwoodirl <christian.carmellini@gmail.com>
2025-05-13 19:25:48 -0400

    removing helpme

--- commands/help.py
+++
@@ -1,9 +0,0 @@
-def run(ctx):
-    if ctx.channel.name == 'lol-queue-pory':
-        help_text = """
-`!newgame` - Creates a new League of Legends lobby. React with role emotes to join and get assigned to balanced teams.
-
-`!link <summoner_name>` - Links your Discord account to your League of Legends account. This helps with team balancing.
-""".trim()
-        return help_text
-    return None
